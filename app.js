32409966c68407d6a888720690bcb2863e2fbba0
Ekunnusi Theophilus <ekunnusitheophilus@gmail.com>
2022-03-13 03:38:54 +0100

    about view implementation via prismic api

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 import {fileURLToPath} from 'url';
 import path from 'path';
+import errorHandler from 'errorhandler'
 
 //prismic
 import * as prismic from '@prismicio/client'
@@ -15,6 +16,8 @@ const __dirname = path.dirname(__filename);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// app middlewares
+app.use(errorHandler())
 app.use((req, res, next) => {
   res.locals.ctx = {
     prismicH,
@@ -24,29 +27,35 @@ app.use((req, res, next) => {
 });
 
 app.get('/', async (req, res) => {
-  const document = await client.get({
-    predicates: prismic.predicate.any('document.type', ['metad', 'home'])
-  })
-  const [home, meta] = document.results
+  const meta = await client.getSingle('metad')
+  const home = await client.getSingle('home')
+
   res.render('pages/home', {home, meta});
 });
 
 app.get('/about', async (req, res) => {
-  const document = await client.get({
-    predicates: prismic.predicate.any('document.type', ['abou', 'metad'])
-  })
-  const [about, meta] = document.results
+  const meta = await client.getSingle('metad')
+  const about = await client.getSingle('abou')
+
   res.render('pages/about', {about, meta});
 });
 
-app.get('/collections', (req, res) => {
-  res.render('pages/collections');
+app.get('/collections', async (req, res) => {
+  const meta = await client.getSingle('metad')
+
+  res.render('pages/collections', {meta});
 });
 
-app.get('/details', (req, res) => {
-  res.render('pages/details');
+app.get('/details/:uid', async (req, res) => {
+  const meta = await client.getSingle('metad')
+  const product = await client.getByUID('product', req.params.uid)
+
+  res.render('pages/details', {meta, product});
 });
 
 app.listen(port, ()=>{
   console.log(`Listening on port ${port}`);
 });
+
+
+
