d5abccedf704172c6c91439986f6da22dff9a95a
Ekunnusi Theophilus <ekunnusitheophilus@gmail.com>
2022-03-14 17:17:53 +0100

    collections, navigation and home page link

--- app.js
+++ app.js
@@ -17,6 +17,25 @@ const __dirname = path.dirname(__filename);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
+// linkresolver
+const handleLinkresolver = (doc) => {
+  console.log(doc)
+
+  if(doc.type === 'product'){
+    return `/details/${doc.slug}`
+  }
+
+  if(doc.type === 'abou'){
+    return `/about`
+  }
+
+  if(doc.type === 'collections'){
+    return `/collections`
+  }
+}
+
+
 // app middlewares
 app.use(errorHandler())
 app.use((req, res, next) => {
@@ -26,49 +45,98 @@ app.use((req, res, next) => {
   res.locals.Increment = (arg) => {
     return arg===0 ? 'One' : arg===1 ? 'Two' : arg===2 ? 'Three' : arg===3 ? 'Four' : ''
   }
+
+  res.locals.Link = handleLinkresolver
+
   next();
 });
 
+// default requirements for every route
+const handleDataReq = async client => {
+  const meta = await client.getSingle('metad')
+  const preloader = await client.getSingle('preloader')
+  const navigation = await client.getSingle('navigation')
+
+  return {
+    meta,
+    navigation,
+    preloader
+  }
+}
+
+// home route ------------------------------------------
 app.get('/', async (req, res) => {
-  const meta = await client.getSingle('metad')
+  const requirements = await handleDataReq(client)
   const home = await client.getSingle('home')
-  const preloader = await client.getSingle('preloader')
+  const { results: collections } = await client.get({
+    predicates: prismic.predicate.at('document.type', 'collection')
+  })
 
-  res.render('pages/home', {home, meta, preloader});
+  res.render('pages/home', {
+    ...requirements,
+    collections,
+    home
+  });
+
 });
+// --------------------------------------------------
+// --------------------------------------------------
 
+
+// about route --------------------------------------
 app.get('/about', async (req, res) => {
-  const meta = await client.getSingle('metad')
+  const requirements = await handleDataReq(client)
   const about = await client.getSingle('abou')
-  const preloader = await client.getSingle('preloader')
 
-  res.render('pages/about', {about, meta, preloader});
+  res.render('pages/about', {
+    ...requirements,
+    about
+  });
+
 });
+// ---------------------------------------------------
+// ---------------------------------------------------
 
+
+// collections --------------------------------------
 app.get('/collections', async (req, res) => {
-  const meta = await client.getSingle('metad')
+  const requirements = await handleDataReq(client)
   const home = await client.getSingle('home')
-  const preloader = await client.getSingle('preloader')
-
   const { results: collections } = await client.get({
     predicates: prismic.predicate.at('document.type', 'collection'),
     fetchLinks: 'product.image'
   })
 
+  res.render('pages/collections', {
+    ...requirements,
+    collections,
+    home
+  });
 
-  res.render('pages/collections', {collections, home, meta, preloader});
 });
+// ---------------------------------------------------
+// ---------------------------------------------------
 
+
+
+// detaile route -----------------------------------
 app.get('/details/:uid', async (req, res) => {
-  const meta = await client.getSingle('metad')
-  const preloader = await client.getSingle('preloader')
+  const requirements = await handleDataReq(client)
   const product = await client.getByUID('product', req.params.uid, {
     fetchLinks: 'collection.title'
   })
 
-  res.render('pages/details', {meta, preloader, product});
-});
+  res.render('pages/details', {
+    ...requirements,
+    product
+  });
 
+});
+// ---------------------------------------------------
+// ---------------------------------------------------
+
+
+// local port listening
 app.listen(port, ()=>{
   console.log(`Listening on port ${port}`);
 });
