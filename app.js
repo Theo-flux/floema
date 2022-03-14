b8b400c96b6e6038595d8da8bcce9a60dc07a6dd
Ekunnusi Theophilus <ekunnusitheophilus@gmail.com>
2022-03-14 14:43:42 +0100

    collections and preloader implementation with prismic

--- app.js
+++ app.js
@@ -23,44 +23,50 @@ app.use((req, res, next) => {
   res.locals.ctx = {
     prismicH,
   };
-
+  res.locals.Increment = (arg) => {
+    return arg===0 ? 'One' : arg===1 ? 'Two' : arg===2 ? 'Three' : arg===3 ? 'Four' : ''
+  }
   next();
 });
 
 app.get('/', async (req, res) => {
   const meta = await client.getSingle('metad')
   const home = await client.getSingle('home')
+  const preloader = await client.getSingle('preloader')
 
-  res.render('pages/home', {home, meta});
+  res.render('pages/home', {home, meta, preloader});
 });
 
 app.get('/about', async (req, res) => {
   const meta = await client.getSingle('metad')
   const about = await client.getSingle('abou')
+  const preloader = await client.getSingle('preloader')
 
-  res.render('pages/about', {about, meta});
+  res.render('pages/about', {about, meta, preloader});
 });
 
 app.get('/collections', async (req, res) => {
   const meta = await client.getSingle('metad')
-  const { results: collection } = await client.get({
+  const home = await client.getSingle('home')
+  const preloader = await client.getSingle('preloader')
+
+  const { results: collections } = await client.get({
     predicates: prismic.predicate.at('document.type', 'collection'),
     fetchLinks: 'product.image'
   })
-  console.log(collection)
-  collection.forEach((el) => {
-    console.log(el.data.product[0].product_products)
-  })
-  res.render('pages/collections', {meta, collection});
+
+
+  res.render('pages/collections', {collections, home, meta, preloader});
 });
 
 app.get('/details/:uid', async (req, res) => {
   const meta = await client.getSingle('metad')
+  const preloader = await client.getSingle('preloader')
   const product = await client.getByUID('product', req.params.uid, {
     fetchLinks: 'collection.title'
   })
 
-  res.render('pages/details', {meta, product});
+  res.render('pages/details', {meta, preloader, product});
 });
 
 app.listen(port, ()=>{
