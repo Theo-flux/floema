e21b6a177dd838d715e1dcdbddacbacf9c130a93
Theo <ekunnusitheophilus@gmail.com>
2023-04-30 06:52:56 +0100

    feat: added prismic access token and repo name

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 // The backend of the whole app
+import path from 'path'
 import express from 'express'
+import { fileURLToPath } from 'url'
+import * as prismicH from '@prismicio/helpers'
+import { client } from './config/prismicConfig.js'
+
 const app = express()
 const port = 3000
 
 app.set('views')
 app.set('view engine', 'pug')
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+app.use(express.static(path.join(__dirname, 'views')))
+
+app.use((req, res, next) => {
+    res.locals.ctx = {
+      prismicH,
+    }
+    next()
+  })
 
 app.get('/', (req, res) => {
     res.render('pages/home')
 })
 
-app.get('/about', (req, res) => {
+app.get('/about', async (req, res) => {
+    const doc = await client.getSingle('abou')
+    console.log(doc)
     res.render('pages/about')
 })
 
