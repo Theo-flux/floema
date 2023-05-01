3f0a73cc2373f716bd55e052edcab108a5ff438b
Theo <ekunnusitheophilus@gmail.com>
2023-05-01 13:38:22 +0100

    feat: about & details page pug layout setup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // The backend of the whole app
 import path from 'path';
 import express from 'express';
+import errorHandler from 'errorhandler';
 import { fileURLToPath } from 'url';
 import * as prismicH from '@prismicio/helpers';
 import { client } from './config/prismicConfig.js';
@@ -12,7 +13,7 @@ app.set('views');
 app.set('view engine', 'pug');
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.join(__dirname, 'views')));
-
+app.use(errorHandler());
 app.use((req, res, next) => {
   res.locals.ctx = {
     prismicH,
@@ -20,31 +21,36 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => {
-  res.render('pages/home');
+app.get('/', async (req, res) => {
+  const meta = await client.getSingle('metad');
+  res.render('pages/home', { meta });
 });
 
 app.get('/about', async (req, res) => {
   const about = await client.getSingle('abou');
   const meta = await client.getSingle('metad');
 
-  about.data.body.forEach(element => {
-    if (element.slice_type === 'highlight') {
-      console.log(element);
-    }
-  });
-  // console.log(about.data.body);
-  // console.log(meta);
-
   res.render('pages/about', { about, meta });
 });
 
-app.get('/collections', (req, res) => {
-  res.render('pages/collections');
+app.get('/collections', async (req, res) => {
+  const meta = await client.getSingle('metad');
+
+  res.render('pages/collections', { meta });
 });
 
-app.get('/details', (req, res) => {
-  res.render('pages/details');
+app.get('/details/:uid', async (req, res) => {
+  const meta = await client.getSingle('metad');
+  const product = await client.getByUID('product', req.params.uid, {
+    fetchLinks: 'collection.title',
+  });
+
+  // console.log(product.data.highlight);
+  // console.log(product.data.info);
+  console.log(product.data.shop);
+  // console.log(req.params.uid);
+
+  res.render('pages/details', { product, meta });
 });
 
 app.listen(port, () => {
